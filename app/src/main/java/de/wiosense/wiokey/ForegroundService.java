82361e5ea1acb26eed16c9312eccfa716faf73a0
Octavio Rodríguez-Castañeda <octavio.rdz@outlook.com>
2025-10-31 19:40:22 -0600

    refactor: Remove EventBus logic from ForegroundService
    - Remove all EventBus subscription methods (`onUiUpdateEvent`, `onDeviceManagementEvent`) and related logic from the `ForegroundService`.
    - Delete methods for handling device management events such as `onReceiveSelectDevice`, `onReceiveRegisterNewDevice`, `onReceiveManualConnectDevice`, `onReceiveDisconnectDevice`, and `onReceiveRemoveDevice`.
    - Remove unused imports, including `EventBus`, `Subscribe`, `BluetoothAdapter`, `Handler`, and `Toast`.
    - Add a try-catch block around the `PendingIntent.getActivity()` call to handle potential exceptions.
    - Simplify a `/**` style comment to `/*`.
    
    Auto-change by Android Studio Gemini-agent

--- app/src/main/java/de/wiosense/wiokey/ForegroundService.java
+++ app/src/main/java/de/wiosense/wiokey/ForegroundService.java
@@ -5,16 +5,12 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothHidDevice;
 import android.bluetooth.BluetoothProfile;
 import android.content.Intent;
-import android.os.Handler;
 import android.os.IBinder;
-import android.os.Looper;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.GuardedBy;
 import androidx.annotation.Nullable;
@@ -22,19 +18,12 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import de.wiosense.wiokey.bluetooth.BluetoothDeviceListing;
-import de.wiosense.wiokey.bluetooth.BluetoothUtils;
 import de.wiosense.wiokey.bluetooth.HidDeviceController;
 import de.wiosense.wiokey.bluetooth.HidDeviceProfile;
-import de.wiosense.wiokey.utils.DeviceManagementEvent;
 import de.wiosense.wiokey.utils.FirebaseManager;
 import de.wiosense.wiokey.utils.UiUpdateEvent;
 
@@ -48,21 +37,18 @@ public class ForegroundService extends Service {
     private static final String TAG = "WioKey|ForegroundService";
     public static final String CHANNEL_ID = "ForegroundServiceChannel";
 
-    private static final int DISCOVERABLE_DURATION_S = 60;
     private static final int REQ_CONNECT_TIMEOUT_MS = 8000;
 
     private static final Object mLock = new Object();
 
     @GuardedBy("mLock")
-    private static boolean mMonitor = false;
-    private static Timer mMonitorTimer = new Timer(Looper.getMainLooper().getThread().getName());
-    private boolean prevConnected = false;
+    private static final boolean mMonitor = false;
 
     private HidDeviceProfile hidDeviceProfile;
     private HidDeviceController hidDeviceController;
     private BluetoothDeviceListing bluetoothDeviceListing;
 
-    private HidDeviceController.ProfileListener profileListener = new HidDeviceController.ProfileListener() {
+    private final HidDeviceController.ProfileListener profileListener = new HidDeviceController.ProfileListener() {
         @Override
         public void onConnectionStateChanged(BluetoothDevice device, int state) {
             synchronized (mLock) {
@@ -92,7 +78,7 @@ public class ForegroundService extends Service {
 
         @Override
         public void onAppStatusChanged(boolean registered) {
-            /**
+            /*
              * If we are registered, we have an active controller and a default device
              * we proceed in try to connect with the default HID host. Since we do not
              * know if the device is in range and this may fail, we need to handle it.
@@ -144,7 +130,12 @@ public class ForegroundService extends Service {
         Log.d(TAG,"Starting Foreground - " + intent);
         createNotificationChannel();
         Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0, notificationIntent, FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = null;
+        try{
+            pendingIntent = PendingIntent.getActivity(this,0, notificationIntent, FLAG_UPDATE_CURRENT);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to get pending intent");
+        }
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setContentTitle(getString(R.string.notification_title_foregroundservice))
                 .setContentText(getString((R.string.notification_body_foregroundservice)))
@@ -226,50 +217,6 @@ public class ForegroundService extends Service {
         notificationManager.notify(2,builder.build());
     }
 
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onUiUpdateEvent(UiUpdateEvent event) {
-        switch (event.mType) {
-            case UPDATE_HID_DEVICES_REQ:
-                Log.d(TAG,"UI -> " + event.mType);
-                updateHidAvailableDevices();
-                break;
-            case UPDATE_DEFAULT_HID_DEVICE_REQ:
-                Log.d(TAG,"UI -> " + event.mType);
-                updateHidDefaultHostDevice();
-                break;
-            default:
-                Log.w(TAG,"Unknown UI updated event " + event.mType);
-        }
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onDeviceManagementEvent(DeviceManagementEvent event) {
-        switch (event.mType) {
-            case SELECT_DEFAULT_DEVICE:
-                Log.d(TAG,"DevMgmt -> " + event.mType + " Device: " + event.mDevice);
-                onReceiveSelectDevice(event.mDevice);
-                break;
-            case REGISTER_NEW_DEVICE:
-                Log.d(TAG,"DevMgmt -> " + event.mType);
-                onReceiveRegisterNewDevice();
-                break;
-            case MANUAL_CONNECT_DEVICE:
-                Log.d(TAG, "DevMgmt -> " + event.mType);
-                onReceiveManualConnectDevice();
-                break;
-            case DISCONNECT_DEVICE:
-                Log.d(TAG, "DevMgmt -> " + event.mType);
-                onReceiveDisconnectDevice();
-                break;
-            case REMOVE_DEVICE:
-                Log.d(TAG, "DevMgmt -> " + event.mType);
-                onReceiveRemoveDevice(event.mDevice);
-                break;
-            default:
-                Log.w(TAG,"Unknown DevMgmt event " + event.mType);
-        }
-    }
-
     private void updateHidDefaultHostDevice(){
         BluetoothDevice defaultDevice = bluetoothDeviceListing.getHidDefaultDevice();
         EventBus.getDefault().post(new UiUpdateEvent(
@@ -287,183 +234,5 @@ public class ForegroundService extends Service {
                     deviceList
             ));
         }
-    }
-
-    private void onReceiveSelectDevice(BluetoothDevice device){
-        if (bluetoothDeviceListing.cacheHidDefaultDevice(device)) {
-            Log.d(TAG, "Updated default preference: " + device);
-            Toast.makeText(this,
-                    getString(R.string.toast_deviceselected,device.getName()),
-                    Toast.LENGTH_LONG)
-                    .show();
-            updateHidDefaultHostDevice();
-            onReceiveManualConnectDevice();
-        } else {
-            Toast.makeText(this,
-                    getString(R.string.toast_deviceselected_fail),
-                    Toast.LENGTH_LONG)
-                    .show();
-        }
-    }
-
-    private void onReceiveRegisterNewDevice(){
-        prevConnected = false;
-
-        synchronized (mLock) {
-            // First check if we are currently connected to the default HID host disconnect immediately
-            if (!mMonitor && hidDeviceController.isConnected()) {
-                hidDeviceController.requestConnect(null);
-                prevConnected = true;
-            }
-
-            // Protect this against future calls until the current one completes
-            if (mMonitor) {
-                Toast.makeText(this,
-                        getString(R.string.toast_deviceregister_repeatedcall),
-                        Toast.LENGTH_LONG)
-                        .show();
-            } else {
-                mMonitor = true;
-                EventBus.getDefault().post(new UiUpdateEvent(
-                        UiUpdateEvent.Type.UPDATE_SCREEN_KEEP_ALIVE_ON
-                ));
-
-                // Schedule new timer
-                mMonitorTimer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        synchronized (mLock) {
-                            mMonitor = false;
-                        }
-                        Log.d(TAG, "Discoverable timer expired");
-
-                        /* Restore connectable but non-discoverable state to ensure privacy */
-                        if (BluetoothUtils.setScanMode(BluetoothAdapter.getDefaultAdapter(),
-                                BluetoothAdapter.SCAN_MODE_CONNECTABLE,
-                                0)) {
-                            Log.d(TAG,getString(R.string.toast_deviceregister_disabled));
-
-                            // Notify user via a toast using event bus and home fragment
-                            EventBus.getDefault().post(new UiUpdateEvent(
-                                    UiUpdateEvent.Type.UPDATE_TOAST,
-                                    getString(R.string.toast_deviceregister_disabled)
-                            ));
-
-                            // Disable screen keepalive lock
-                            EventBus.getDefault().post(new UiUpdateEvent(
-                                    UiUpdateEvent.Type.UPDATE_SCREEN_KEEP_ALIVE_OFF
-                            ));
-                        }
-
-                        /* If we are not connected at this point, but we were before reconnect */
-                        if (!hidDeviceController.isConnected() && prevConnected) {
-                            hidDeviceController.requestConnect(bluetoothDeviceListing.getHidDefaultDevice(),
-                                    REQ_CONNECT_TIMEOUT_MS);
-                        } else {
-                            // Otherwise simply refresh UI state based on current HID
-                            BluetoothDevice device = bluetoothDeviceListing.getHidDefaultDevice();
-                            EventBus.getDefault().post(new UiUpdateEvent(
-                                    UiUpdateEvent.Type.UPDATE_CONN_ANIMATION,
-                                    device,
-                                    hidDeviceProfile.getConnectionState(device)
-                            ));
-                        }
-                    }
-                }, DISCOVERABLE_DURATION_S * 1000);
-
-                // Here just make the device discoverable for the next minute or so to allow user to pair to PC
-                BluetoothUtils.setScanMode(BluetoothAdapter.getDefaultAdapter(),
-                        BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE,
-                        DISCOVERABLE_DURATION_S);
-
-                // Notify user via a toast using event bus and home fragment
-                EventBus.getDefault().post(new UiUpdateEvent(
-                        UiUpdateEvent.Type.UPDATE_TOAST,
-                        getString(R.string.toast_deviceregister_enabled,DISCOVERABLE_DURATION_S)));
-            }
-            
-            // Rerender a delayed animation showing that we are waiting for a request to pair
-            final Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    EventBus.getDefault().post(new UiUpdateEvent(
-                            UiUpdateEvent.Type.UPDATE_CONN_ANIMATION,
-                            null,
-                            BluetoothProfile.STATE_CONNECTING
-                    ));
-                }
-            }, 500);
-        }
-    }
-
-    private void onReceiveManualConnectDevice() {
-        hidDeviceController.requestConnect(bluetoothDeviceListing.getHidDefaultDevice(),
-                REQ_CONNECT_TIMEOUT_MS);
-        EventBus.getDefault().post(new UiUpdateEvent(
-                UiUpdateEvent.Type.UPDATE_TOAST,
-                getString(R.string.toast_devicemanual_connect,
-                        Objects.requireNonNull(bluetoothDeviceListing.getHidDefaultDevice()).getName())
-        ));
-    }
-
-    private void onReceiveDisconnectDevice() {
-        hidDeviceController.requestConnect(null);
-        EventBus.getDefault().post(new UiUpdateEvent(
-                UiUpdateEvent.Type.UPDATE_TOAST,
-                getString(R.string.toast_device_disconnected)
-        ));
-    }
-
-    private void onReceiveRemoveDevice(BluetoothDevice device) {
-        // 0. Check if device is null -> do nothing if so
-        // 1. If connected to this device -> disconnect
-        // 2.0 If device was default host -> clear clearHidPreference(BluetoothDevice device, String HID_PREFERRED_HOST)
-        // 2.1 Remove device from HID devices list
-        // 3. Call the removeBond(device) method from Bluetooth
-        // 4. Post event to update UI on the Event Bus for UPDATE_HID_DEVICES
-        // 5. end
-
-        if (device == null) {
-            Log.e(TAG, "Cannot remove null device");
-            return;
-        }
-
-        if (hidDeviceProfile != null && hidDeviceController.isConnected() &&
-                hidDeviceProfile.getConnectionState(device) == BluetoothProfile.STATE_CONNECTED) {
-            hidDeviceController.requestConnect(null);
-        }
-
-        if (bluetoothDeviceListing != null && bluetoothDeviceListing.clearHidDevice(device)) {
-            Log.d(TAG, "Cleared HID device");
-            EventBus.getDefault().post(new UiUpdateEvent(
-                    UiUpdateEvent.Type.UPDATE_TOAST,
-                    getString(R.string.toast_devicecleared)
-            ));
-        } else {
-            Log.e(TAG, "Failed to clear HID device from cache");
-        }
-
-        if (BluetoothUtils.removeBond(device)) {
-            EventBus.getDefault().post(new UiUpdateEvent(
-                    UiUpdateEvent.Type.UPDATE_TOAST,
-                    getString(R.string.toast_deviceunpaired,device.getAddress())
-            ));
-        } else {
-            EventBus.getDefault().post(new UiUpdateEvent(
-                    UiUpdateEvent.Type.UPDATE_TOAST,
-                    getString(R.string.toast_deviceunpaired_failed, device.getAddress())
-            ));
-        }
-
-        // Post delayed UI event update to allow sync with sharedPreferences
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                updateHidDefaultHostDevice();
-                updateHidAvailableDevices();
-            }
-        }, 100);
     }
 }
