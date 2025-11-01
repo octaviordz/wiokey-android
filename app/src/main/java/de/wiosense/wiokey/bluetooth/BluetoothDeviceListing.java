851b59d92929b039b1d28b92f38772e99e0fdccd
Octavio Rodríguez-Castañeda <octavio.rdz@outlook.com>
2025-10-31 19:44:48 -0600

    WIP refactor: Comment out unused permission check in BluetoothDeviceListing
    - Comments out a placeholder `BLUETOOTH_CONNECT` permission check in the `getAvailableDevices` method.
    - The commented code includes a `TODO` for handling permission requests.

--- app/src/main/java/de/wiosense/wiokey/bluetooth/BluetoothDeviceListing.java
+++ app/src/main/java/de/wiosense/wiokey/bluetooth/BluetoothDeviceListing.java
@@ -82,6 +82,16 @@ public class BluetoothDeviceListing {
 
     @SuppressLint("RestrictedApi")
     public List<BluetoothDevice> getAvailableDevices() {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return TODO;
+//        }
         Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
         List<BluetoothDevice> availableDevices = new ArrayList<>();
 
